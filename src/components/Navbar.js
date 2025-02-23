c02fae415157556ce7634250ac780de04477fc3e
Anjali <163436616+Anjaligupta90@users.noreply.github.com>
2025-02-23 13:29:21 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import logoimage from '../assets/logoimage.png'
 
 const Navbar = () => {
   return (
     <div className="navbar flex justify-between items-center p-4 font-bold">
       <div className="logo_container flex items-center">
-        <img src="assets/NavLogo.jpg" alt="Logo" className="logo w-20" />
+        <img src={logoimage} alt="Logo" className="logo w-[50px] rounded-full h-[50px]" />
         <div className="logo_text text-2xl">Anjali Gupta</div>
       </div>
       <div className="nav_items flex gap-8">
-        <div><a href="#projects" className="text-lg">Projects</a></div>
-        <div><a href="#skills" className="text-lg">Skills</a></div>
-        <div><a href="#contact" className="text-lg">Contact Me</a></div>
+        <div><a href="#projects" className="text-lg hover:text-red-500">Projects</a></div>
+        <div><a href="#skills" className="text-lg hover:text-red-500">Skills</a></div>
+        <div><a href="#contact" className="text-lg hover:text-red-500">Contact Me</a></div>
       </div>
     </div>
   );
